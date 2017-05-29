f7ec094e31b122e0950cfaf705d24a255b070317
Lukasz Dudek <lukasz.j.dudek@uj.edu.pl>
2017-05-29 16:34:04 +0200

    Removed initialisation of costate equations; simulation with them is working.

--- tests/reachability_set.py
+++ tests/reachability_set.py
@@ -4,29 +4,46 @@ from numpy.ma import arange
 from pyfmi.fmi import load_fmu
 
 from pymodelica import compile_fmu
-from tests.run_model_optimisation import get_model_path, simulate
+from tests.run_model_optimisation import get_model_path, simulate, U_MAX
 
 
 class ReachabilitySetsCalculator:
     def __init__(self):
         self.model_path = get_model_path()
 
-    def initialise_costate_equations(self, theta, phi):
-        init_fmu = compile_fmu("TanksPkg.ThreeTanksCostateEquationsInitialisation", self.model_path)
+    def get_model_with_costate_equations(self):
+        init_fmu = compile_fmu("TanksPkg.ThreeTanksCostateEquations", self.model_path)
         init_model = load_fmu(init_fmu)
+        return init_model
+
+    def initialise_costate_equations(self, init_model, theta, phi):
         init_model.set('phi', phi)
         init_model.set('theta', theta)
         init_model.initialize()
-        return init_model.get(['psi1', 'psi2', 'psi3'])
+        return_param = init_model.get(['psi1', 'psi2', 'psi3'])
+        init_model.reset()
+        return return_param
 
-    def get_reachability_set(self, precision=.01):
+    def get_reachability_set(self, precision=.1):
+        init_model = self.get_model_with_costate_equations()
         for phi in arange(0, 2*pi, precision):
             for theta in arange(0, 2*pi, precision):
-                [psi10, psi20, psi30] = self.initialise_costate_equations(theta, phi)
-                print psi10, psi20, psi30
+                init_model.reset()
+                init_model.set('phi', phi)
+                init_model.set('theta', theta)
+                init_res = self.single_simulation(init_model)
+                print init_model.get_log()
 
-    def single_simulation(self):
-        h1_init_sim, h2_init_sim, h3_init_sim, init_res = simulate(self.model_path)
+    def single_simulation(self, init_sim_model=None, t_start=0.0, t_final=50.0):
+        if not init_sim_model:
+            init_sim_fmu = compile_fmu("TanksPkg.ThreeTanksCostateEquations", self.model_path)
+            # Load the model
+            init_sim_model = load_fmu(init_sim_fmu)
+        # Set initial and reference values
+        init_sim_model.set('u', U_MAX)  # TODO: develop a way to include real u calculation here
+        # Simulate with constant input Tc
+        init_res = init_sim_model.simulate(start_time=t_start, final_time=t_final)
+        return init_res
 
 
 def main():
