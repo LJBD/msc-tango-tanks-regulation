5c2982594f5be53e6def1131aed67d14c34b2e3e
Lukasz Dudek <lukasz.j.dudek@uj.edu.pl>
2017-07-08 16:01:39 +0200

    Corrected optimising with DS script.

--- tests/optimise_with_ds.py
+++ tests/optimise_with_ds.py
@@ -1,28 +1,31 @@
 from __future__ import print_function
 from time import sleep
 
-from PyTango import DeviceProxy, DevState
+try:
+    from tango import DeviceProxy, DevState, ApiUtil
+except ImportError:
+    from PyTango import DeviceProxy, DevState, ApiUtil
 from numpy import linspace
 
-from ds_tanks.tanks_utils import plot_with_optimal_trajectories
+from ds_tanks.tanks_utils import plot_with_optimal_trajectories, plot_results
 
 
 def main(with_commands=True):
     opt_dev = DeviceProxy("opt/ctrl/1")
     if with_commands:
         print("Setting initial values...")
-        # opt_dev.command_inout_asynch("GetEquilibriumFromControl", 88.5)
-        # sleep(5)
         opt_dev.write_read_attribute("H1Final", 15)
         opt_dev.write_read_attribute("H2Final", 15)
-        opt_dev.write_read_attribute("H3Final", 12)
-        # print("Running simulation...")
-        # opt_dev.command_inout_asynch("RunSimulation")
-        # sleep(5)
+        opt_dev.write_read_attribute("H3Final", 9)
+
         print("Optimising...")
         opt_dev.command_inout_asynch("Optimise")
         sleep(20)
+
+        opt_dev.command_inout("GetLQR", timeout=5)
         opt_dev.command_inout_asynch("SendControl")
+        opt_dev.command_inout("NormaliseOptimalControl")
+
     if opt_dev.state() == DevState.STANDBY:
         opt_control = opt_dev.read_attribute("OptimalControl").value
         opt_time = opt_dev.read_attribute("OptimalTime").value
@@ -30,9 +33,10 @@ def main(with_commands=True):
         opt_h1 = opt_dev.read_attribute("OptimalH1").value
         opt_h2 = opt_dev.read_attribute("OptimalH2").value
         opt_h3 = opt_dev.read_attribute("OptimalH3").value
-        # plot_results(opt_h1, opt_h2, opt_h3, time, opt_control,
-        #              "Optimised with TanksOptimalControl")
-        # opt_dev.command_inout_asynch("RunVerification")
+        plot_results(opt_h1, opt_h2, opt_h3, time_opt, opt_control,
+                     "Optimised with TanksOptimalControl")
+        # Running verification...
+        opt_dev.command_inout_asynch("RunVerification")
         h1_sim = opt_dev.read_attribute("H1Simulated").value
         h2_sim = opt_dev.read_attribute("H2Simulated").value
         h3_sim = opt_dev.read_attribute("H3Simulated").value
@@ -47,6 +51,7 @@ def main(with_commands=True):
 
 def get_matlab_data(device):
     switch_times = device.read_attribute('SwitchTimes').value
+    k = device.read_attribute("K").value
     data = [device.read_attribute('H1Final').value,
             device.read_attribute('H2Final').value,
             device.read_attribute('H3Final').value,
@@ -54,9 +59,13 @@ def get_matlab_data(device):
             device.read_attribute('OptimalControl').value[0],
             0,
             switch_times[0],
-            switch_times[1]]
+            switch_times[1],
+            80.16,
+            k[0],
+            k[1],
+            k[2]]
     return data
 
 
 if __name__ == '__main__':
-    main(True)
+    main(False)
