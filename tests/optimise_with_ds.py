4c6e2784de6cacba0b7cf1c6f75128b8a740251d
Lukasz Dudek <lukasz.j.dudek@uj.edu.pl>
2017-07-09 19:47:12 +0200

    Added another argument to optimise with DS script and changed the main method's name.

--- tests/optimise_with_ds.py
+++ tests/optimise_with_ds.py
@@ -10,16 +10,16 @@ from numpy import linspace
 from ds_tanks.tanks_utils import plot_with_optimal_trajectories, plot_results
 
 
-def main(with_commands=True):
+def run_optimisation_through_ds(with_commands=True, with_plots=True):
     opt_dev = DeviceProxy("opt/ctrl/1")
     if with_commands:
         q = [[0.1, 0.0, 0.0],
              [0.0, 0.1, 0.0],
              [0.0, 0.0, 0.1]]
         print("Setting initial values...")
-        print("H1 final:", opt_dev.write_read_attribute("H1Final", 15).value)
-        print("H2 final:", opt_dev.write_read_attribute("H2Final", 15).value)
-        print("H3 final:", opt_dev.write_read_attribute("H3Final", 9).value)
+        print("H1 final:", opt_dev.write_read_attribute("H1Final", 16).value)
+        print("H2 final:", opt_dev.write_read_attribute("H2Final", 16).value)
+        print("H3 final:", opt_dev.write_read_attribute("H3Final", 15).value)
         print("Control weight:", opt_dev.write_read_attribute("R", 0.01).value)
         print("State weights:", opt_dev.write_read_attribute("Q", q).value)
 
@@ -47,17 +47,19 @@ def main(with_commands=True):
         opt_h1 = opt_dev.read_attribute("OptimalH1").value
         opt_h2 = opt_dev.read_attribute("OptimalH2").value
         opt_h3 = opt_dev.read_attribute("OptimalH3").value
-        plot_results(opt_h1, opt_h2, opt_h3, time_opt, opt_control,
-                     "Optimised with TanksOptimalControl")
+        if with_plots:
+            plot_results(opt_h1, opt_h2, opt_h3, time_opt, opt_control,
+                         "Optimised with TanksOptimalControl")
 
         h1_sim = opt_dev.read_attribute("H1Simulated").value
         h2_sim = opt_dev.read_attribute("H2Simulated").value
         h3_sim = opt_dev.read_attribute("H3Simulated").value
         time_sim = opt_dev.read_attribute("SimulationTime").value
         print(get_matlab_data(opt_dev))
-        plot_with_optimal_trajectories(time_opt, time_sim, h1_sim, h2_sim,
-                                       h3_sim, opt_h1, opt_h2, opt_h3,
-                                       opt_control)
+        if with_plots:
+            plot_with_optimal_trajectories(time_opt, time_sim, h1_sim, h2_sim,
+                                           h3_sim, opt_h1, opt_h2, opt_h3,
+                                           opt_control)
     else:
         print("Wrong state, exiting!")
 
@@ -81,4 +83,4 @@ def get_matlab_data(device):
 
 
 if __name__ == '__main__':
-    main(True)
+    run_optimisation_through_ds(True)
